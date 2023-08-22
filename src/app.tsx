b3f2a42a06ec8aa7eb32d882d83bd2f22750aa68
underwritter <ntpp96@mail.ru>
2023-08-22 13:52:32 +0300

    init teamplate weather app

---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      style={{
+        padding: "20px",
+        border: "solid 2px red",
+        color: "white",
+        background: "black",
+      }}
+    >
+      react setup from scratch without cra testing ts"
+    </div>
+  );
+}
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,52 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {test: /\.(html)$/, use: ["html-loader"]},
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
